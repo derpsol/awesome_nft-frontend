7f20ce516b906d6518d23a410497e24814ecc763
derpsol <franklyjin029@gmail.com>
2023-03-03 04:38:56 -0800

    awesome nft frontend first commit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Box, Container, Typography } from "@mui/material";
+import logo from "../../assets/images/logo.png";
+
+function Home() {
+  return (
+    <Container maxWidth='xl'>
+      <Box
+        component="img"
+        src={logo}
+        width='128px'
+        height='128px'
+        m='12px'
+      />
+      <Typography
+        textTransform='uppercase'
+      >
+        awesome_nft.fi
+      </Typography>
+    </Container>
+  );
+}
+
+export default Home;
